4d93e55749083e0c3c538f57d20026b55d4a7c5b
daniyar <daniyardautbaev2104@gmail.com>
2023-02-20 16:53:31 +0600

    lab4

---
+++ lab 4/math/task_4.py
@@ -0,0 +1,8 @@
+
+def parallelogram_area(height, lenght):
+    return height*lenght
+
+h = int(input("Length of base: "))
+l = int(input("Height of parallelogram: "))
+
+print("Area of parallelogram:",parallelogram_area(h, l))
